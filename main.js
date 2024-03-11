3c74b43aafb9c7be827083221aeca1cf8a1897a0
ProfWender <163004792+ProfWender@users.noreply.github.com>
2024-03-11 14:01:59 -0300

    Construção do main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+let nome_usuario = "";
+let elemento = document.querySelector("#nome-usuário");
+
+
+while(nome_usuario == ""){
+    nome_usuario = prompt("Qual é o seu nome? ");
+}
+
+if(nome_usuario == null){
+    elemento.textContent = "bem-vindo!";
+}else{
+    elemento.textContent = nome_usuario;
+}
+
